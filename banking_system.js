1d3d46da3ec11e128a3168fb2f0fd6ebcc8f52f2
DevonLoen <devonloen99@gmail.com>
2024-09-27 23:11:25 +0700

    Feat : Add/import Modules

---
+++ banking_system.js
@@ -0,0 +1,8 @@
+const readline = require("readline");
+const { StandardCustomer,PriorityCustomer} = require('./bank_account.js');
+// Inisialisasi readline untuk terminal input/output
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}); 
+
